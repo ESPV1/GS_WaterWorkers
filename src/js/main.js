8e142241b61fe969d51a1935e8facbffdd59d863
git config --global user.name Gabriel <gabriel.amara@hotmail.com>
2025-06-06 18:04:02 -0300

    Mudança de tema

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import { quiz } from "./quiz.js"
 import { tecnologiesDescription } from "./cardsDesc.js";
 import { slideshow } from "./slideShow.js";
+import { toggleTheme } from "./theme.js";
+import { headerChange } from "./header.js";
 
+toggleTheme();
+headerChange();
 tecnologiesDescription();
 quiz();
 slideshow();
