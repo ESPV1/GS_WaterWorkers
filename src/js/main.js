a91a6d1b0cd7ad637094358157a87b06b6c76873
git config --global user.name Gabriel <gabriel.amara@hotmail.com>
2025-06-06 17:39:48 -0300

    Slide show funcional

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import { quiz } from "./quiz.js"
 import { tecnologiesDescription } from "./cardsDesc.js";
+import { slideshow } from "./slideShow.js";
 
 tecnologiesDescription();
 quiz();
+slideshow();
